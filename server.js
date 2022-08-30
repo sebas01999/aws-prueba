a0b390bc4e6c79cb12986440a7c49c3befef33ac
Sebastian Salamanca <zebaz1999@outlook.es>
2022-08-30 14:19:37 -0500

    cambio de server estatic2o

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const swaggerSpec={
         },
         servers:[
             {
-                url:"http://http://52.21.31.95:3000"
+                url:"http://52.21.31.95:3000"
             },
         ],
     },
