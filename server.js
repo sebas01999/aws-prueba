b1e0d393e3d711fc7dcbb12634393825725f0fd4
Sebastian Salamanca <zebaz1999@outlook.es>
2022-08-30 16:11:20 -0500

    cors

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express= require('express');
 const app= express();
 const mongoose= require("mongoose")
-//const cors= require('cors')
+const cors= require('cors')
 const routes=require('./rutas')
 const path=require("path")
 
@@ -33,7 +33,7 @@ mongoose.connect('mongodb+srv://sebastian1999:SG99201st@cluster0.1aem6ri.mongodb
 app.use(express.json())
 app.use('/api',routes)
 app.use("/api-doc", swaggerUI.serve, swaggerUI.setup(swaggerJsDoc(swaggerSpec)))
-//app.use(cors())
+app.use(cors())
 
 //rutas......
 
