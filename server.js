f3f640dc24c5141cd1c8d7e0a473573f574a4c02
Sebastian Salamanca <zebaz1999@outlook.es>
2022-09-05 08:23:12 -0500

    prueba 1

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ mongoose.connect('mongodb+srv://sebastian1999:SG99201st@cluster0.1aem6ri.mongodb
 .catch((error) => console.error(error))
 
 app.use(express.json())
-app.use('/api',routes)
+app.use('/api',cors(corsOptions),routes)
 app.use("/api-doc", swaggerUI.serve, swaggerUI.setup(swaggerJsDoc(swaggerSpec)))
 app.use(cors(corsOptions))
 
@@ -43,7 +43,7 @@ app.use(cors(corsOptions))
 //rutas......
 
 
-app.get('/',(req, res)=>{
+app.get('/',cors(corsOptions),(req, res)=>{
     res.send('bienvenido a mi API')
 })
 
