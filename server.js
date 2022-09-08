831b8da4fbb81c9abb9cf8b59b51808bcb33ffd6
Sebastian Salamanca <zebaz1999@outlook.es>
2022-09-08 18:03:43 -0500

    prueba de xd2

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose= require("mongoose");
 
 const routes=require('./rutas');
 const path=require("path");
-const whitelist='http://ec2-3-88-114-158.compute-1.amazonaws.com';
+const whitelist='http://3.88.114.158:3000';
 const corsOptions = {
 	origin: whitelist,
     optionsSuccessStatus: 200
@@ -30,13 +30,27 @@ const swaggerSpec={
 };
 
 app.set('port', process.env.PORT || 3000);
-
+var corsOptionsDelegate = function (req, callback) {
+    const corsOptions = {
+        methods: ["GET", "PUT", "POST", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true
+    };
+  
+    const myIpAddress = req.connection.remoteAddress; // This is where you get the IP address from the request
+    if (whitelist.indexOf(myIpAddress) !== -1) {
+        corsOptions.origin = true
+    } else {
+        corsOptions.origin = false
+    }
+    callback(null, corsOptions);
+}
 //conexion a mongodb
 mongoose.connect('mongodb+srv://sebastian1999:SG99201st@cluster0.1aem6ri.mongodb.net/?retryWrites=true&w=majority').then(() => console.log("Conectadooo")) // utilizamos la varibale ambiente .env
 .catch((error) => console.error(error));
 app.use(cors(corsOptions));
 app.use(express.json());
-app.use('/api',cors(corsOptions),routes);
+app.use('/api',cors(corsOptionsDelegate),routes);
 app.use("/api-doc",swaggerUI.serve, swaggerUI.setup(swaggerJsDoc(swaggerSpec)));
 
 
