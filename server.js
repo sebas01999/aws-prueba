8d9fdc0749f1817c98a820e77ef4bc5dc1098eea
Sebastian Salamanca <zebaz1999@outlook.es>
2022-09-08 17:27:38 -0500

    prueba de xd2

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-const express= require('express')
+const express= require('express');
+const cors= require('cors');
 const app= express();
-const mongoose= require("mongoose")
+const mongoose= require("mongoose");
 
-const routes=require('./rutas')
-const path=require("path")
-const whitelist='http://3.88.114.158:3000'
+const routes=require('./rutas');
+const path=require("path");
+const whitelist='http://3.88.114.158:3000';
 const corsOptions = {
 	origin: whitelist,
 	optionsSuccessStatus: 200
 };
 //swagger
-const swaggerUI=require("swagger-ui-express")
-const swaggerJsDoc=require("swagger-jsdoc")
+const swaggerUI=require("swagger-ui-express");
+const swaggerJsDoc=require("swagger-jsdoc");
 const swaggerSpec={
     definition:{
         openapi:"3.0.0",
@@ -26,17 +27,17 @@ const swaggerSpec={
         ],
     },
     apis:[`${path.join(__dirname, "./rutas.js")}`]
-}
+};
 
 app.set('port', process.env.PORT || 3000);
 
 //conexion a mongodb
 mongoose.connect('mongodb+srv://sebastian1999:SG99201st@cluster0.1aem6ri.mongodb.net/?retryWrites=true&w=majority').then(() => console.log("Conectadooo")) // utilizamos la varibale ambiente .env
-.catch((error) => console.error(error))
-
-app.use(express.json())
-app.use('/api',routes)
-app.use("/api-doc",swaggerUI.serve, swaggerUI.setup(swaggerJsDoc(swaggerSpec)))
+.catch((error) => console.error(error));
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use('/api',cors(corsOptions),routes);
+app.use("/api-doc",swaggerUI.serve, swaggerUI.setup(swaggerJsDoc(swaggerSpec)));
 
 
 
@@ -44,7 +45,7 @@ app.use("/api-doc",swaggerUI.serve, swaggerUI.setup(swaggerJsDoc(swaggerSpec)))
 
 
 app.get('/',(req, res)=>{
-    res.send('bienvenido a mi API')
+    res.send('bienvenido a mi API');
 })
 
 
